61dc4391c2b0158c2904bfdac4a0b9317e590ebf
AjayiMike <ajayimikeofficial@gmail.com>
2022-01-26 07:45:27 +0100

    add onlyOwner modifier to the mint function

--- contracts/SkateToken.sol
+++ contracts/SkateToken.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 contract SkateToken is ERC20 {
   address private _owner;
+  
   constructor(uint256 totalSupply) ERC20("SkateToken", "SKT") {
     _owner = msg.sender;
     _mint(msg.sender, totalSupply * 1e18);
@@ -12,7 +15,20 @@ contract SkateToken is ERC20 {
     return _owner;
   }
 
-  function mint(address _to,  uint256 _amount) public {
+  modifier onlyOwner {
+    require(msg.sender == _owner, "Skate contract: Unauthorized!");
+    _;
+  }
+
+  function setOwner(address _newOwner) public onlyOwner {
+    _owner = _newOwner;
+  }
+
+  function mint(address _to,  uint256 _amount) public onlyOwner {
     _mint(_to, _amount);
   }
+
+  receive() external payable {}
 }
+
+
